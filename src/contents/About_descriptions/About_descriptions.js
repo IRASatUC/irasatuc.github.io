c216d9fb55a08d9beaa5621975d7cb3ebad45dbe
hoang <nguye3hg@mail.uc.edu>
2024-07-29 15:09:56 -0400

    fix about description

--- src/contents/About_descriptions/About_descriptions.js
+++ src/contents/About_descriptions/About_descriptions.js
@@ -1,4 +1,4 @@
 export const About_descriptions=[
-    "IRAS Lab is a 2000-sqft lab facility at UC main campus for supporting research and teaching in multidisciplinary areas of robotics, artificial intelligence, and autonomous systems for space, aviation and other applications.",
+    "IRAS Lab is a 4000-sqft lab facility at UC main campus for supporting research and teaching in multidisciplinary areas of robotics, artificial intelligence, and autonomous systems for space, aviation and other applications.",
     "IRAS Lab"
 ]
