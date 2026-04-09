d5d14b9d97063ea8e72e82e4ddf2023bbb5f5520
dochiphi <dochiphi2208@gmail.com>
2026-04-09 11:38:29 -0400

    Update News Layout and ProfileUI button

--- src/contents/News_contents/News_contents.js
+++ src/contents/News_contents/News_contents.js
@@ -1,30 +1,50 @@
+import ExploringRoboticsCareers from "../../components/News/img/ExploringRoboticsCareers.png";
+import SSTI from "../../components/News/img/spdm_on_ssrms.jpg";
+import SpaceForceSatellite from "../../components/News/img/140717ssc0661_900xx5760-3240-0-300.jpg";
+import SpaceForceSelections from "../../components/News/img/211123-F-GO452-0001F.jpg";
+import SatellitesThatFixOtherSatellites from "../../components/News/img/ouma7.jpeg";
+import PushFoward from "../../components/News/img/push-forward-commercial-tess.jpeg";
+import RobotOpeningDoor from "../../components/News/img/door_bot_hGRbGxwmWf.avif";
+
 export const News_contents = [
     {
         link:"https://www.uc.edu/news/articles/2025/03/exploring-careers-in-robotics-engineering--a-path-to-the-future.html",
-        title:"Robotics engineering is not just exciting — it’s lucrative. According to Glassdoor.com, salaries for robotics engineers in the U.S. range from $119,000 to $201,000 annually, with a median salary of $154,000 in total pay. The demand is strong, too: the robotics industry is projected to grow significantly over the next decade, driven by advancements in AI, automation and space exploration. For UC grads, Cincinnati’s proximity to tech hubs and manufacturing giants offers a prime launchpad.",
+        title:"Robotics engineering is not just exciting — it’s lucrative.",
+        summary: "According to Glassdoor.com, salaries for robotics engineers in the U.S. range from $119,000 to $201,000 annually, with a median salary of $154,000 in total pay. The demand is strong, too: the robotics industry is projected to grow significantly over the next decade, driven by advancements in AI, automation and space exploration. For UC grads, Cincinnati’s proximity to tech hubs and manufacturing giants offers a prime launchpad.",
+        image: ExploringRoboticsCareers,
         date:"Mar 28, 2025"
     },
-    {
+
+   /*{
         link:"",
-        title:"Our team member, Andrew Barth, recently completed his PhD dissertation and defense. Andy, who graduated with a degree in Aerospace Engineering from UC in 1999, brings 15 years of experience in the space industry, including contributions to the ISS robotics and Orion programs. He returned to UC a few years ago to pursue doctoral research in Space Robotics. With his PhD now completed, Andy will concentrate on advancing research for the USSF SSTI2 project at the IRAS Lab.",
+        title:"Our team member, Andrew Barth, recently completed his PhD dissertation and defense.",
+        summary:"Andy, who graduated with a degree in Aerospace Engineering from UC in 1999, brings 15 years of experience in the space industry, including contributions to the ISS robotics and Orion programs. He returned to UC a few years ago to pursue doctoral research in Space Robotics. With his PhD now completed, Andy will concentrate on advancing research for the USSF SSTI2 project at the IRAS Lab.",
+        image: ,
         date:"Nov 17, 2024"
-    },
+    },*/
+
     {
         link:"https://www.spaceforce.mil/News/Article-Display/Article/3736639/ussf-announces-award-to-texas-am-and-university-of-cincinnati-for-space-strateg/",
         title:"USSF announces award to Texas A&M and University of Cincinnati for Space Strategic Technology Institute 2",
+        summary:"The U.S. Space Force has awarded cooperative agreements to Texas A&M University and University of Cincinnati to fund research and technology development related to In-Space Operations for up to $37.6 million and $11.5 million, respectively. The awards will be executed over a five-year period of performance.",
+        image: SSTI,
         date:"April 10, 2024"
     },
     {
         link:"https://www.bizjournals.com/cincinnati/news/2024/01/12/university-cincinnati-space-command-satellite.html",
         title:"U.S. Space Force selects UC to lead satellite research project",
+        image: SpaceForceSatellite,
+        summary: "The University of Cincinnati will conduct breakthrough research on in-space operations thanks to an $11.5 million grant from the U.S. Space Force and the Air Force Research Laboratory.",
         date:"Jan 13, 2024"
     },
     {
         link:"https://www.spaceforce.mil/News/Article-Display/Article/3634897/ussf-announces-selections-for-space-strategic-technology-institute-2/",
         title:"USSF announces selections for Space Strategic Technology Institute 2",
+        image: SpaceForceSelections,
+        summary: "The United States Space Force selected two institutions under the Space Strategic Technology Institute, or SSTI 2, to facilitate applied research in the areas of In-Space Operations, which includes Space Access, Mobility, and Logistics, or SAML.",
         date:"Jan. 8, 2024"
     },
-    {
+    /*{
         link:"https://www.uc.edu/news/articles/2023/09/uc-engineer-designs-robots-to-fix-satellites.html",
         title:"UC engineer designs robots to fix satellites",
         date:"October 2, 2023"
@@ -38,18 +58,24 @@ export const News_contents = [
         link:"https://www.uc.edu/news/articles/2021/08/uc-engineers-independent-robots-that-can-move-your-couch.html",
         title:"These robots can move your couch",
         date:"August 23, 2021"
-    },
+    },*/
     {
         link:"https://www.ispot.tv/ad/tnNv/university-of-cincinnati-next-level",
         title:"University of Cincinnati TV Spot, 'Push Forward'",
+        image: PushFoward,
+        summary: "The University of Cincinnati's new TV spot, 'Push Forward,' highlights the university's commitment to innovation and progress. The ad features a montage of students and faculty engaged in cutting-edge research and creative projects across various disciplines, showcasing the vibrant campus life and the spirit of pushing boundaries to achieve greatness.",
         date:"October 31, 2020"
     },
     {   link:"https://www.hackster.io/news/university-of-cincinnati-s-robot-can-locate-outlets-and-open-doors-5c45016d5389",
         title:"University of Cincinnati's Robot Can Locate Outlets and Open Doors",
+        summary: "Researchers at the University of Cincinnati have developed a robot capable of locating electrical outlets and opening doors, showcasing advancements in robotics and artificial intelligence. The robot utilizes advanced sensors and machine learning algorithms to navigate complex environments, making it a significant step forward in the field of autonomous robotics.",
+        image: RobotOpeningDoor,
         date:"3 years ago"
     },
     {   link:"https://www.uc.edu/news/articles/2019/11/n20877097.html",
         title:"UC develops satellites that fix other satellites",
+        image: SatellitesThatFixOtherSatellites,
+        summary: "University of Cincinnati professor Ou Ma is engineering robotics technology to fix orbiting satellites in his Intelligent Robotics and Autonomous Systems Lab. He envisions robotic satellites that can dock with other satellites for repairs or refueling.",
         date:"November 26, 2019"
     },
 ];
